8559243d8a56a741be1471c2e900a6732c4663be
xyauhideto <xyauhideto@gmail.com>
2017-05-03 21:31:16 +0800

    新增兑换称号，自定义内容，更多功能开关

--- 515 daily.user.js
+++ 515 daily.user.js
@@ -1,6 +1,6 @@
 ﻿// ==UserScript==
 // @name         515 daily
-// @version      0.2.1
+// @version      0.3
 // @description  2017起点515活动自动
 // @author       xyau
 // @downloadURL       https://github.com/xyauhideto/qidian/raw/master/515%20daily.user.js
@@ -9,32 +9,50 @@
 // @include *www.yuewen.com/app*
 // @include *acts.qidian.com/2017/5813823*
 // ==/UserScript==
-//自定义参数,是为1，否为0
+
+//功能开关,是为1，否为0
+//投票对象
+author = '蝴蝶蓝';
 //自动投赞赏
 autoVote = 1;
+//赞赏全投，否则投1张
+voteAll = 1;
+//自动兑称号
+autoExchange = 1;
+//自动发贴分享
+autoShare = 1;
 //领取礼包
 getBag = 1;
+//自动回答问题
+autoAnswer = 1;
+//自定义回答内容
+answer = '233';
+//自动开宝球
+autoOpen = 1;
 //赞赏、宝球自动刷新
 autoRefresh = 0;
 //自动刷新间隔，单位毫秒
 interval = 2500;
+
 window.addEventListener('DOMContentLoaded', 
                         function() {
 url = location.href;
-    // 自动投赞赏
-if(autoVote==1&&/godLink/i.test(url)){n=$('.name'); for(var i=0;i<n.length;i++){if(/蝴蝶蓝/.test(n[i].innerText)){n[i].nextElementSibling.nextElementSibling.firstChild.click();break;}} if(parseInt($('.my-vote-number')[0].innerText)>0){$("a[title='充值']").slice(-1)[0].click();$("a[data-click='voteApply']")[0].click();}if(autoRefresh==1)setTimeout('location.reload()', interval)}
+    // 投赞赏
+if(autoVote==1&&/godLink/i.test(url)){eval("$('a[data-info*="+author+"]')[0].click()");if($('.my-vote-number')[0].innerText>0){if(voteAll==1)$("a[title='充值']").slice(-1)[0].click();$("a[data-click='voteApply']")[0].click();}if(autoRefresh==1)setTimeout('location.reload()', interval);}
+	// 兑称号
+if(autoExchange==1&&/godLink/i.test(url)){eval("$('a[data-info*="+author+"]')[1].click()");$("a[data-click='toExchange']")[0].click();}
     if(getBag==1){
-    // 自动领礼包并发帖
-if(/jhMeet/i.test(url)){bu=$('li[data-type]');var j=0;function r(){bu[j].click();$('a[title="领取"]')[1].click();j++;if(j==2)clearInterval(e);}var e=setInterval(r,500);be=$('li[data-num]');var k=0;function re(){be[k].click();$('a[title="领取"]')[0].click();k++;if(k==4)clearInterval(ee);}var ee=setInterval(re,500);setTimeout(function(){$('a[title="发帖"]')[0].click();},2500);}
+    // 领礼包并发帖
+if(autoShare==1&&/jhMeet/i.test(url)){bu=$('li[data-type]');var j=0;function r(){bu[j].click();$('a[title="领取"]')[1].click();j++;if(j==2)clearInterval(e);}var e=setInterval(r,500);be=$('li[data-num]');var k=0;function re(){be[k].click();$('a[title="领取"]')[0].click();k++;if(k==4)clearInterval(ee);}var ee=setInterval(re,500);setTimeout(function(){$('a[title="发帖"]')[0].click();},2500);}
 } else{
-    // 仅自动发帖
-if(/jhMeet/i.test(url))$('a[title="发帖"]')[0].click();
+    // 仅发帖
+if(autoShare==1&&/jhMeet/i.test(url))$('a[title="发帖"]')[0].click();
 }
     // 从APP推广页后退
     if(!/515/i.test(url))location.href='http://activity.qidian.com/2017/515/jhMeet';
-    // 自动回问答
-    if(/faceToFace/i.test(url)){$('#myAnswerContent')[0].value="233";$('button')[0].click();}
-    // 自动开宝球
-if(/orbLib/i.test(url)){$('a.btn').slice(-1)[0].click();if(autoRefresh==1)setTimeout('location.reload()', interval)}
+    // 回问答
+if(autoAnswer==1&&/faceToFace/i.test(url)){$('#myAnswerContent')[0].value=answer;$('button')[0].click();}
+    // 开宝球
+if(autoOpen==1&&/orbLib/i.test(url)){$('a.btn').slice(-1)[0].click();if(autoRefresh==1)setTimeout('location.reload()', interval)}
 },
   false);
