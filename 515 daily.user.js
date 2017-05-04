03d4b4d94929237619f36d16c0347f92c9e8934d
xyauhideto <xyauhideto@gmail.com>
2017-05-04 22:24:16 +0200

    时间间隔调整

--- 515 daily.user.js
+++ 515 daily.user.js
@@ -1,6 +1,6 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name         515 daily
-// @version      0.3.1
+// @version      0.3.2
 // @description  2017起点515活动自动
 // @author       xyau
 // @downloadURL       https://github.com/xyauhideto/qidian/raw/master/515%20daily.user.js
@@ -30,9 +30,9 @@ answer = '233';
 //自动开宝球
 autoOpen = 1;
 //赞赏、宝球自动刷新
-autoRefresh = 0;
+autoRefresh = 1;
 //自动刷新间隔，单位毫秒
-interval = 2500;
+interval = 1500;
 
 window.addEventListener('DOMContentLoaded', 
                         function() {
@@ -43,7 +43,7 @@ if(autoVote==1&&/godLink/i.test(url)){eval("$('a[data-info*="+author+"]')[0].cli
 if(autoExchange==1&&/godLink/i.test(url)){eval("$('a[data-info*="+author+"]')[1].click()");$("a[data-click='toExChange']")[0].click();}
     if(getBag==1){
     // 领礼包并发帖
-if(autoShare==1&&/jhMeet/i.test(url)){bu=$('li[data-type]');var j=0;function r(){bu[j].click();$('a[title="领取"]')[1].click();j++;if(j==2)clearInterval(e);}var e=setInterval(r,500);be=$('li[data-num]');var k=0;function re(){be[k].click();$('a[title="领取"]')[0].click();k++;if(k==4)clearInterval(ee);}var ee=setInterval(re,500);setTimeout(function(){$('a[title="发帖"]')[0].click();},2500);}
+if(autoShare==1&&/jhMeet/i.test(url)){bu=$('li[data-type]');var j=0;function r(){bu[j].click();$('a[title="领取"]')[1].click();j++;if(j==2)clearInterval(e);}var e=setInterval(r,200);be=$('li[data-num]');var k=0;function re(){be[k].click();$('a[title="领取"]')[0].click();k++;if(k==4)clearInterval(ee);}var ee=setInterval(re,200);setTimeout(function(){$('a[title="发帖"]')[0].click();},1000);}
 } else{
     // 仅发帖
 if(autoShare==1&&/jhMeet/i.test(url))$('a[title="发帖"]')[0].click();
@@ -53,6 +53,6 @@ if(autoShare==1&&/jhMeet/i.test(url))$('a[title="发帖"]')[0].click();
     // 回问答
 if(autoAnswer==1&&/faceToFace/i.test(url)){$('#myAnswerContent')[0].value=answer;$('button')[0].click();}
     // 开宝球
-if(autoOpen==1&&/orbLib/i.test(url)){$('a.btn').slice(-1)[0].click();if(autoRefresh==1)setTimeout('location.reload()', interval)}
+if(autoOpen==1&&/orbLib/i.test(url)){$('a.btn').slice(-1)[0].click();if(autoRefresh==1)setTimeout('location.reload()', interval*2)}
 },
   false);
