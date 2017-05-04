d8990fbba9e5c177f95c69e4a47b31a929f74093
xyauhideto <xyauhideto@gmail.com>
2017-05-04 19:55:35 +0800

    修复兑换bug

--- 515 daily.user.js
+++ 515 daily.user.js
@@ -1,6 +1,6 @@
 ﻿// ==UserScript==
 // @name         515 daily
-// @version      0.3
+// @version      0.3.1
 // @description  2017起点515活动自动
 // @author       xyau
 // @downloadURL       https://github.com/xyauhideto/qidian/raw/master/515%20daily.user.js
@@ -40,7 +40,7 @@ url = location.href;
     // 投赞赏
 if(autoVote==1&&/godLink/i.test(url)){eval("$('a[data-info*="+author+"]')[0].click()");if($('.my-vote-number')[0].innerText>0){if(voteAll==1)$("a[title='充值']").slice(-1)[0].click();$("a[data-click='voteApply']")[0].click();}if(autoRefresh==1)setTimeout('location.reload()', interval);}
 	// 兑称号
-if(autoExchange==1&&/godLink/i.test(url)){eval("$('a[data-info*="+author+"]')[1].click()");$("a[data-click='toExchange']")[0].click();}
+if(autoExchange==1&&/godLink/i.test(url)){eval("$('a[data-info*="+author+"]')[1].click()");$("a[data-click='toExChange']")[0].click();}
     if(getBag==1){
     // 领礼包并发帖
 if(autoShare==1&&/jhMeet/i.test(url)){bu=$('li[data-type]');var j=0;function r(){bu[j].click();$('a[title="领取"]')[1].click();j++;if(j==2)clearInterval(e);}var e=setInterval(r,500);be=$('li[data-num]');var k=0;function re(){be[k].click();$('a[title="领取"]')[0].click();k++;if(k==4)clearInterval(ee);}var ee=setInterval(re,500);setTimeout(function(){$('a[title="发帖"]')[0].click();},2500);}
