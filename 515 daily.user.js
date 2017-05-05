a645b1b427eb592b5734f7d1d7b27ea7c8c1a0bf
xyauhideto <xyauhideto@gmail.com>
2017-05-05 21:39:34 +0200

    增加兑称号、江湖交流发帖回退延迟，优化自动性能

--- 515 daily.user.js
+++ 515 daily.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         515 daily
-// @version      0.3.2
+// @version      0.3.3
 // @description  2017起点515活动自动
 // @author       xyau
 // @downloadURL       https://github.com/xyauhideto/qidian/raw/master/515%20daily.user.js
@@ -16,7 +16,7 @@ author = '蝴蝶蓝';
 //自动投赞赏
 autoVote = 1;
 //赞赏全投，否则投1张
-voteAll = 1;
+voteAll = 0;
 //自动兑称号
 autoExchange = 1;
 //自动发贴分享
@@ -40,7 +40,7 @@ url = location.href;
     // 投赞赏
 if(autoVote==1&&/godLink/i.test(url)){eval("$('a[data-info*="+author+"]')[0].click()");if($('.my-vote-number')[0].innerText>0){if(voteAll==1)$("a[title='充值']").slice(-1)[0].click();$("a[data-click='voteApply']")[0].click();}if(autoRefresh==1)setTimeout('location.reload()', interval);}
 	// 兑称号
-if(autoExchange==1&&/godLink/i.test(url)){eval("$('a[data-info*="+author+"]')[1].click()");$("a[data-click='toExChange']")[0].click();}
+if(autoExchange==1&&/godLink/i.test(url)){setTimeout(function(){eval("$('a[data-info*="+author+"]')[1].click()");$("a[data-click='toExChange']")[0].click();},autoVote==1?500:0);}
     if(getBag==1){
     // 领礼包并发帖
 if(autoShare==1&&/jhMeet/i.test(url)){bu=$('li[data-type]');var j=0;function r(){bu[j].click();$('a[title="领取"]')[1].click();j++;if(j==2)clearInterval(e);}var e=setInterval(r,200);be=$('li[data-num]');var k=0;function re(){be[k].click();$('a[title="领取"]')[0].click();k++;if(k==4)clearInterval(ee);}var ee=setInterval(re,200);setTimeout(function(){$('a[title="发帖"]')[0].click();},1000);}
@@ -49,7 +49,7 @@ if(autoShare==1&&/jhMeet/i.test(url)){bu=$('li[data-type]');var j=0;function r()
 if(autoShare==1&&/jhMeet/i.test(url))$('a[title="发帖"]')[0].click();
 }
     // 从APP推广页后退
-    if(!/515/i.test(url))location.href='http://activity.qidian.com/2017/515/jhMeet';
+    if(!/515/i.test(url))setTimeout("location.href='https://activity.qidian.com/2017/515/jhMeet';",1500);
     // 回问答
 if(autoAnswer==1&&/faceToFace/i.test(url)){$('#myAnswerContent')[0].value=answer;$('button')[0].click();}
     // 开宝球
