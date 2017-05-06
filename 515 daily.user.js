3dbb91701130b420dce1b30b76b3e85f9c9d1f9e
xyauhideto <xyauhideto@gmail.com>
2017-05-06 15:36:27 +0200

    礼包延迟调整

--- 515 daily.user.js
+++ 515 daily.user.js
@@ -43,7 +43,7 @@ if(autoVote==1&&/godLink/i.test(url)){eval("$('a[data-info*="+author+"]')[0].cli
 if(autoExchange==1&&/godLink/i.test(url)){setTimeout(function(){eval("$('a[data-info*="+author+"]')[1].click()");$("a[data-click='toExChange']")[0].click();},autoVote==1?500:0);}
     if(getBag==1){
     // 领礼包并发帖
-if(autoShare==1&&/jhMeet/i.test(url)){bu=$('li[data-type]');var j=0;function r(){bu[j].click();$('a[title="领取"]')[1].click();j++;if(j==2)clearInterval(e);}var e=setInterval(r,200);be=$('li[data-num]');var k=0;function re(){be[k].click();$('a[title="领取"]')[0].click();k++;if(k==4)clearInterval(ee);}var ee=setInterval(re,200);setTimeout(function(){$('a[title="发帖"]')[0].click();},1000);}
+if(autoShare==1&&/jhMeet/i.test(url)){bu=$('li[data-type]');var j=0;function r(){bu[j].click();$('a[title="领取"]')[1].click();j++;if(j==2)clearInterval(e);}var e=setInterval(r,200);be=$('li[data-num]');var k=0;function re(){be[k].click();$('a[title="领取"]')[0].click();k++;if(k==4)clearInterval(ee);}var ee=setInterval(re,200);setTimeout(function(){$('a[title="发帖"]')[0].click();},1500);}
 } else{
     // 仅发帖
 if(autoShare==1&&/jhMeet/i.test(url))$('a[title="发帖"]')[0].click();
