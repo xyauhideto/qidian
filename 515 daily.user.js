e44446531a619a069f3fb8e2396f8c5284854543
xyauhideto <xyauhideto@gmail.com>
2017-05-06 22:16:29 +0200

    禁止交换名帖页自动运行

--- 515 daily.user.js
+++ 515 daily.user.js
@@ -38,15 +38,15 @@ window.addEventListener('DOMContentLoaded',
                         function() {
 url = location.href;
     // 投赞赏
-if(autoVote==1&&/godLink/i.test(url)){eval("$('a[data-info*="+author+"]')[0].click()");if($('.my-vote-number')[0].innerText>0){if(voteAll==1)$("a[title='充值']").slice(-1)[0].click();$("a[data-click='voteApply']")[0].click();}if(autoRefresh==1&&getBag==1)setTimeout('location.reload()', interval);}
+if(autoVote==1&&/godLink/i.test(url)){eval("$('a[data-info*="+author+"]')[0].click()");if($('.my-vote-number')[0].innerText>0){if(voteAll==1)$("a[title='充值']").slice(-1)[0].click();$("a[data-click='voteApply']")[0].click();}if(autoRefresh==1)setTimeout('location.reload()', interval);}
 	// 兑称号
 if(autoExchange==1&&/godLink/i.test(url)){setTimeout(function(){eval("$('a[data-info*="+author+"]')[1].click()");$("a[data-click='toExChange']")[0].click();},autoVote==1?500:0);}
     if(getBag==1){
     // 领礼包并发帖
-if(autoShare==1&&/jhMeet/i.test(url)){bu=$('li[data-type]');var j=0;function r(){bu[j].click();$('a[title="领取"]')[1].click();j++;if(j==2)clearInterval(e);}var e=setInterval(r,200);be=$('li[data-num]');var k=0;function re(){be[k].click();$('a[title="领取"]')[0].click();k++;if(k==4)clearInterval(ee);}var ee=setInterval(re,200);setTimeout(function(){$('a[title="发帖"]')[0].click();},1500);}
+if(autoShare==1&&/jhMeet/i.test(url)&&!/share/i.test(url)){bu=$('li[data-type]');var j=0;function r(){bu[j].click();$('a[title="领取"]')[1].click();j++;if(j==2)clearInterval(e);}var e=setInterval(r,1200);be=$('li[data-num]');var k=0;function re(){be[k].click();$('a[title="领取"]')[0].click();k++;if(k==4)clearInterval(ee);}var ee=setInterval(re,1200);setTimeout(function(){$('a[title="发帖"]')[0].click();},6000);}
 } else{
     // 仅发帖
-if(autoShare==1&&/jhMeet/i.test(url))$('a[title="发帖"]')[0].click();
+if(autoShare==1&&/jhMeet/i.test(url)&&!/share/i.test(url))$('a[title="发帖"]')[0].click();
 }
     // 从APP推广页后退
     if(!/515/i.test(url))setTimeout("location.href='https://activity.qidian.com/2017/515/jhMeet';",1500);
