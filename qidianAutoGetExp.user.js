5d9dc47af70ffd3a77351821a313df7b7d0b7982
xyauhideto <xyauhideto@gmail.com>
2017-08-15 13:58:30 +0200

    Update qidianAutoGetExp.user.js

--- qidianAutoGetExp.user.js
+++ qidianAutoGetExp.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         qidianAutoGetExp
 // @namespace    https://greasyfork.org/users/10290
-// @version      0.1
+// @version      0.2
 // @description  自动领取起点经验、活跃度礼包
 // @author       xyau
 // @match        http://my.qidian.com/*
@@ -10,70 +10,71 @@
 // @downloadURL       https://github.com/xyauhideto/qidian/raw/master/qidianAutoGetExp.user.js
 // @grant        none
 // ==/UserScript==
-// 签到论坛书号
-var bids=[24857, 88071, 1039430, 1887208, 3206900];
-// 访问书友UID
-var uids=[157811195,124536726,5393042,1129498,103646169];
 window.addEventListener('load', function() {
     'use strict';
-    var url=location.href;
-    if(/forum/.test(url)){
-        if(bids.indexOf(parseInt(g_data.bid))>-1)document.querySelector('.forum-sign').click();
+    var url = location.href;
+    if (/forum/.test(url)) {
+        if (bids.indexOf(parseInt(g_data.bid)) > -1) document.querySelector('.forum-sign').click();
     }
     // 在线经验
-    if(/level/.test(url)){
-        function rE(){
-            var a=document.querySelector("a.elGetExp");
-            if(a){
+    if (/level/.test(url)) {
+        function rE() {
+            var a = document.querySelector("a.elGetExp");
+            if (a) {
                 a.click();
-                eE=clearInterval(eE);
+                eE = clearInterval(eE);
             }
         }
-        var eE=setInterval(rE,100);
+        var eE = setInterval(rE, 100);
         document.getElementById('elOldexp').click();
-        function rO(){
-            var o=document.querySelector('.ui-dialog .ui-button');
-            if(o){
+
+        function rO() {
+            var o = document.querySelector('.ui-dialog .ui-button');
+            if (o) {
                 o.click();
                 document.querySelector('.ui-dialog-close').click();
-                eO=clearInterval(eO);
+                eO = clearInterval(eO);
             }
         }
-        var eO=setInterval(rO,100);
+        var eO = setInterval(rO, 100);
     }
     // 活跃度礼包
-    if(document.querySelector('#elActive')){
-        function rA(){
-            if(!document.querySelector('.ui-loading-icon')){
-                document.querySelectorAll("a.elGetGift").forEach(function(e){
+    if (document.querySelector('#elActive')) {
+        function rA() {
+            if (!document.querySelector('.ui-loading-icon')) {
+                document.querySelectorAll("a.elGetGift").forEach(function(e) {
                     e.click();
                 });
-                eA=clearInterval(eA);
+                eA = clearInterval(eA);
             }
         }
-        var eA=setInterval(rA,100);
+        var eA = setInterval(rA, 100);
     }
     // 天天打卡
-    if(/signeveryday/.test(url)){
+    if (/signeveryday/.test(url)) {
         var params = "content=&type=3&img=a&random=";
         new Ajax("/Ajax/SignHandler.ashx",
-                 params,
-                 function(){
-            var objresult = eval(arguments[0]);
-            if (objresult.result){
-                location.reload(true);
-                uids.forEach(function(e){
-                    window.open('http://my.qidian.com/user/'+e);
-                });
-                bids.forEach(function(e){
-                    window.open('http://forum.qidian.com/NewForum/List.aspx?forumId='+e);
-                });
-            }else{
-                TipsDialog('提示', objresult.returnString, '');
-                obj.setAttribute("onclick", "AddSign(this)");
-            }
-        },
-                 "post",
-                 "addusersign");
+            params,
+            function() {
+                var objresult = eval(arguments[0]);
+                if (objresult.result) {
+                    location.reload(true);
+                    // 签到论坛书号
+                    var cbids = [22661933000779000, 22322981000467400, 22344706000284500, 22300167000689400, 22277657000074300];
+                    // 访问书友UID
+                    var uids = [157811195, 124536726, 5393042, 1129498, 103646169];
+                    uids.forEach(function(e) {
+                        window.open('http://my.qidian.com/user/' + e);
+                    });
+                    bids.forEach(function(e) {
+                        window.open('http://forum.qidian.com/index/' + e);
+                    });
+                } else {
+                    TipsDialog('提示', objresult.returnString, '');
+                    obj.setAttribute("onclick", "AddSign(this)");
+                }
+            },
+            "post",
+            "addusersign");
     }
-},false);
+}, false);
