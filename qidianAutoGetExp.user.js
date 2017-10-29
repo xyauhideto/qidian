47f75500b7c466a5618c0e96b3a7d46ff94b4cdc
xyauhideto <xyauhideto@gmail.com>
2017-10-29 22:45:41 +0100

    论坛签到改用ajax; 修改旧经验领取的bug; 活跃度改用ajax在经验页完成

--- qidianAutoGetExp.user.js
+++ qidianAutoGetExp.user.js
@@ -1,33 +1,39 @@
 // ==UserScript==
 // @name         qidianAutoGetExp
 // @namespace    https://greasyfork.org/users/10290
-// @version      1.3.20171029
+// @version      1.3.2017103117
 // @description  自动领取起点经验、活跃度礼包
 // @author       xyau
-// @match        http*://my.qidian.com/*
+// @match        http*://my.qidian.com/level*
 // @match        http*://bbs.qidian.com/signeveryday.aspx*
 // @match        http*://forum.qidian.com/index/*
 // @downloadURL       https://github.com/xyauhideto/qidian/raw/master/qidianAutoGetExp.user.js
 // @grant		GM_getValue
 // @grant		GM_setValue
-// @grant		GM_deleteValue
+// @run-at		document-start
 // ==/UserScript==
-window.addEventListener('load', function() {
+window.addEventListener('load', () => {
 	'use strict';
-	const guid = +/ywguid=(\d+);/.exec(document.cookie)[1],
+	const guid = +/ywguid=(\d+)(?:;|$)/.exec(document.cookie)[1],
+		  _csrfToken = /_csrfToken=(\w+)(?:;|$)/.exec(document.cookie)[1],
 		  date = new Date(new Date().getTime() + 288e5).toJSON().substr(0, 10),
-		  url = location.href,
-		  bids = [24857, 88071, 1039430, 1887208, 3206900];
+		  url = location.pathname,
+		  uids = [157811195,124536726,5393042,1129498,103646169], // 访问书友UID
+		  bids = [24857, 88071, 1039430, 1887208, 3206900]; //论坛签到书号
 
-	if (/forum/.test(url) && bids.includes(+g_data.bid)) {
-		var eS = $('.blue-btn');
-		if (eS.hasClass('forum-sign'))
-			eS.click();
-		}
+	if (/index/.test(url) && bids.includes(+g_data.bid) && !$('.blue-btn').hasClass('signed'))
+		$.ajax({
+			url: '/ajax/my/BookForum/signIn',
+			type: 'post',
+			data: {
+				_csrfToken,
+				forumId:  g_data.forumId,
+			},
+		}).then(r=>$('.blue-btn').append(r.msg));
 
 	// 在线经验
 	if (/level/.test(url)) {
-		var eE = $('#elTaskWrap a');
+		let eE = $('#elTaskWrap a');
 		if (eE.length) {
 			let text = eE.text();
 			if (text === '领取')
@@ -41,12 +47,12 @@ window.addEventListener('load', function() {
 		// 旧经验
 		let lastGet = GM_getValue(guid);
 		if (!lastGet || date != lastGet) {
-			$.each([157811195,124536726,5393042,1129498,103646169], (_, uid) =>	open('https://my.qidian.com/user/'+uid));// 访问书友UID
-			$.each(bids, (_, bid) => open('https://forum.qidian.com/NewForum/List.aspx?forumId='+bid));// 论坛签到
+			uids.forEach(uid =>	open('https://my.qidian.com/user/' + uid));
+			bids.forEach(bid => open('https://forum.qidian.com/NewForum/List.aspx?forumId=' + bid));
 			$.ajax({
 				url: '/ajax/score/ExchangeOld',
 				type: 'post',
-				data: /(_csrfToken=\w+)(?:;|$)/.exec(document.cookie)[1],
+				data: {_csrfToken},
 			}).then((r) => {
 				$('#elOldexp').append(r.msg);
 				console.log(r);
@@ -54,26 +60,28 @@ window.addEventListener('load', function() {
 					GM_setValue(guid, date);
 			});
 		}
-	}
 
-	// 活跃度礼包
-	var eA = $('#elTaskPackage'),
-		getGift = () => {
-			var eAB = $("a.elGetGift").toArray();
-			if (eAB.length)
-				$.each(eAB, (_, e) => e.click());
-		};
-	if (eA && eA.length) {
-		getGift();
-		let oA = new MutationObserver((ms) => ms.forEach(getGift));
-		oA.observe(eA[0], {attributes: true});
+		// 活跃度礼包
+		$.ajax({
+			url: '/ajax/userActivity/mission',
+			type: 'get',
+			data: {_csrfToken},
+		}).then(r => r.data.bagList.filter(bag => bag.status === 1).forEach(bag =>
+																			$.ajax({
+			url: '/ajax/userActivity/take',
+			type: 'post',
+			data: {
+				_csrfToken,
+				bagId: bag.bagId,
+			},
+		}).then(r => $('#elTaskButton').after(`<p>领取活跃度${bag.score}礼包${r.data.award}</p>`))));
 	}
 
 	// 天天打卡
 	if (/signeveryday/.test(url) && document.querySelector('.BDtop2left3 img').src.includes('2')) {
 		new Ajax('/Ajax/SignHandler.ashx',
-				 'content=&type=3&img=a&random=',
-				 (r) => document.querySelector('.BDtop2right').append(/result:(\d+)/g.exec(r)[1] ? '签到成功' : /'(.+)'/.exec(r)[1]),
+				 {type: 3, img: 'a'},
+				 r => document.querySelector('.BDtop2right').append(/result:(\d+)/g.exec(r)[1] ? '签到成功' : /'(.+)'/.exec(r)[1]),
 				 'post',
 				 'addusersign');
 	}
